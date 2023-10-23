3b5d50b6e68ac4a77518670e6b030994af221092
Рывкин Александр <ryvkin.aleksandr@wildberries.work>
2023-10-23 20:10:34 +0300

    1

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class MyClient(discord.Client):
             await message.reply("I am an idiot bot! XD", mention_author=True)
 
 intents = discord.Intents.default()
+intents.members = True
 intents.message_content = True
 
 client = MyClient(intents=intents)
