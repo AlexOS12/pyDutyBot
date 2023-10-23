0b27808323e450a4b678f6248518373e1290b2ee
AlexOS12 <sluhach2016@gmail.com>
2023-10-23 20:29:56 +0300

    Обновил бота

--- app/main.py
+++ app/main.py
@@ -6,16 +6,22 @@ class MyClient(discord.Client):
         print('-'*10)  
         pass
 
-    async def on_message(self, message):
+    async def on_message(self, message: discord.message.Message):
         if message.author.id == self.user.id:
             pass
 
         if message.content.startswith('/whoami'):
+            print(message.author.name)
             await message.reply("I am an idiot bot! XD", mention_author=True)
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
+token : str
+
+with open('token.txt', 'r') as file:
+    token = file.read().replace('\n', '')
+
 client = MyClient(intents=intents)
-client.run('token')
+client.run(token)
