e63b5566d2a1fa3eb36bed4fcf6d9b472d5ccd86
AlexOS12 <sluhach2016@gmail.com>
2023-10-23 18:34:20 +0300

    Создал базового бота

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-if __name__ == "__main__":
-    print("Hello World!")
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f"Logged in as {self.user} (ID: {self.user.id})")
+        print('-'*10)  
+        pass
+
+    async def on_message(self, message):
+        if message.author.id == self.user.id:
+            pass
+
+        if message.content.startswith('/whoami'):
+            await message.reply("I am an idiot bot! XD", mention_author=True)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('token')
