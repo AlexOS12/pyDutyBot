f7d1f8c6e5bfe35de70d5f92ba4e6586059267d3
AlexOS12 <sluhach2016@gmail.com>
2023-10-24 16:29:12 +0300

    Начал работу над обновлением бота (см. app_new)

---
+++ app_new/parser.py
@@ -0,0 +1,30 @@
+import discord
+
+def parse(msg : discord.message.Message) -> None:
+    preparedMessage = prepareMessage(msg.content)
+    isInTrustedList(msg.author)
+    return preparedMessage
+
+def prepareMessage(msg : str) -> str:
+    msg = msg[2:].strip().lower()
+    msg = ' '.join(msg.split())
+    return msg
+
+# тут у нас будут команды
+# пока что сделал просто заглушки (почти все)
+
+# Проверяем, что пользователь находится в доверенном списке
+def isInTrustedList(member: discord.Member) -> bool:
+    if not(trustedList):
+        return True
+    return True if member.name in trustedList else False
+
+# Лист доверенных юзеров
+trustedList : list[str]
+
+with open("trusted_list.txt", 'r') as file:
+    trustedList = [i.replace('\n', '') for i in file.readlines()]
+    print("Доверенный список юзеров:")
+    for i in range(len(trustedList)):
+        print(f"{i + 1} - {trustedList[i]}")
+   
